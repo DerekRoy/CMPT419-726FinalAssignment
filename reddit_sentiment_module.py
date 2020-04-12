265553024fbc82a7c4c8d2ed12e079aac38be6bc
Derek <derek.r.roy@gmail.com>
2020-04-12 14:38:36 -0700

    Remove superfluous classifiers, adapt Vote system
    Removed the unneeded classifiers, and adapted vote system to work with two classifiers and include neutral label

--- reddit_sentiment_module.py
+++ reddit_sentiment_module.py
@@ -20,7 +20,10 @@ class VoteSystem(ClassifierI):
         for classifier in self._classifiers:
             vote = classifier.classify(featureset)
             votes.append(vote)
-        return mode(votes), votes.count(mode(votes))/len(votes)
+        try:
+            return mode(votes)
+        except:
+            return "ntrl"
 
 
 curr_file = open("pickles/lexicon.pickle", "rb")
@@ -47,39 +50,9 @@ multinomial_nb_classifier = pickle.load(curr_file)
 curr_file.close()
 print("multinomial naive bayes classifier loaded")
 
-curr_file = open("pickles/bernoulli_nb_twitter.pickle", "rb")
-bernoulli_nb_classifier = pickle.load(curr_file)
-curr_file.close()
-print("bernoulli naive bayes classifier loaded")
-
-curr_file = open("pickles/sgdc_twitter.pickle", "rb")
-sgdc_classifier = pickle.load(curr_file)
-curr_file.close()
-print("sgdc classifier loaded")
-
-curr_file = open("pickles/svc_twitter.pickle", "rb")
-svc_classifier = pickle.load(curr_file)
-curr_file.close()
-print("svc classifier loaded")
-
-curr_file = open("pickles/linear_svc_twitter.pickle", "rb")
-linear_svc_classifier = pickle.load(curr_file)
-curr_file.close()
-print("linear svc classifier loaded")
-
-curr_file = open("pickles/nu_svc_twitter.pickle", "rb")
-nu_svc_classifier = pickle.load(curr_file)
-curr_file.close()
-print("nu svc classifier loaded")
-
 classifier = VoteSystem(
     naive_bayes_classifier,
     multinomial_nb_classifier,
-    bernoulli_nb_classifier,
-    sgdc_classifier,
-    svc_classifier,
-    linear_svc_classifier,
-    nu_svc_classifier
 )
 
 
